e9be78d621e4255788edd75a09ba2b58d8f5af9e
Dipanshi293 <155524086+Dipanshi293@users.noreply.github.com>
2024-01-08 18:10:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,71 @@
 
+
+variable "project_id" {
+  description = "Bucket project id."
+  type        = string
+  default = "eighth-breaker-407305"
+}
+
+variable "location" {
+  description = "Bucket location."
+  type        = string
+  default     = "EU"
+}
+
+variable "name" {
+  default = "dbag-gcs"
+  type        = string
+}
+
+variable "storage_class" {
+  description = "Bucket storage class."
+  type        = string
+  default     = "MULTI_REGIONAL"
+  validation {
+    condition     = contains(["STANDARD", "MULTI_REGIONAL", "REGIONAL", "NEARLINE", "COLDLINE", "ARCHIVE"], var.storage_class)
+    error_message = "Storage class must be one of STANDARD, MULTI_REGIONAL, REGIONAL, NEARLINE, COLDLINE, ARCHIVE."
+  }
+}
+
+variable "force_destroy" {
+  description = "Optional map to set force destroy keyed by name, defaults to false."
+  type        = bool
+  default     = false
+}
+
+variable "uniform_bucket_level_access" {
+  description = "Allow using object ACLs (false) or not (true, this is the recommended behavior) , defaults to true (which is the recommended practice, but not the behavior of storage API)."
+  type        = bool
+  default     = true
+}
+
+variable "labels" {
+  description = "Labels to be attached to all buckets."
+  type        = map(string)
+  default     = {}
+}
+
+variable "default_event_based_hold" {
+  description = "Enable event based hold to new objects added to specific bucket, defaults to false."
+  type        = bool
+  default     = null
+}
+
+variable "requester_pays" {
+  description = "Enables Requester Pays on a storage bucket."
+  type        = bool
+  default     = null
+}
+
+variable "public_access_prevention" {
+  description = "Prevents public access to a bucket. Acceptable values are inherited or enforced. If inherited, the bucket uses public access prevention, only if the bucket is subject to the public access prevention organization policy constraint."
+  type        = string
+  default     = null
+}
+
+variable "versioning" {
+  description = "Enable versioning, defaults to false."
+  type        = bool
+  default     = false
+}
+
