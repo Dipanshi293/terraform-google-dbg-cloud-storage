0915ab0cbd52517321746220fc1a3291f4ccdf32
Dipanshi293 <155524086+Dipanshi293@users.noreply.github.com>
2024-01-09 18:51:03 +0530

    add lables,default event based hold and requester pays

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ resource "google_storage_bucket" "bucket" {
   storage_class               = var.storage_class
   force_destroy               = var.force_destroy
   uniform_bucket_level_access = var.uniform_bucket_level_access
-  #labels                      = var.labels
-  #default_event_based_hold    = var.default_event_based_hold
-  #requester_pays              = var.requester_pays
+  labels                      = var.labels
+  default_event_based_hold    = var.default_event_based_hold
+  requester_pays              = var.requester_pays
   public_access_prevention    = var.public_access_prevention
   versioning {
     enabled = var.versioning
